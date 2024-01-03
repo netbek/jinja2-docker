625343fbbfbd5aa982f40f1b3b033f5b4d4f45b2
Hein Bekker <hein@netbek.co.za>
2024-01-03 07:29:44 +0200

    Add debug option

--- app/render.py
+++ app/render.py
@@ -10,6 +10,7 @@ PRETTIER_PARSER_CHOICES = ("yaml",)
 
 if __name__ == "__main__":
     template = os.environ.get("TEMPLATE")
+    debug = os.environ.get("DEBUG", "0").lower() in ("1", "true")
     pretty = os.environ.get("PRETTY", "0").lower() in ("1", "true")
     prettier_parser = os.environ.get("PRETTIER_PARSER")
 
@@ -29,6 +30,15 @@ if __name__ == "__main__":
     for arg in sys.argv[1:]:
         key, value = arg.split("=", 1)
         context[key] = value
+
+    if debug:
+        sys.stdout.write("Configuration\n")
+        sys.stdout.write("-------------\n")
+        sys.stdout.write(f"template: {template}\n")
+        sys.stdout.write(f"debug: {debug}\n")
+        sys.stdout.write(f"pretty: {pretty}\n")
+        sys.stdout.write(f"prettier_parser: {prettier_parser}\n")
+        sys.stdout.write(f"context: {context}\n\n")
 
     env = Environment(loader=FileSystemLoader(TEMPLATES_DIR))
     template = env.get_template(template)
